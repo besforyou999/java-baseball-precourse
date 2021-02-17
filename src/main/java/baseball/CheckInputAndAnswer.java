3059aa9d4d642268aef3e04cc15120ea74e5b54a
besforyou <besforyou2@gmail.com>
2021-02-16 23:51:40 -0800

    feat(src/main/java/baseball/) check how many strikes and balls occured
    Methods to check how many strikes and balls occured
    added to class checkInputAndAnswer.java

--- src/main/java/baseball/CheckInputAndAnswer.java
+++ src/main/java/baseball/CheckInputAndAnswer.java
@@ -2,11 +2,36 @@ package baseball;
 import java.util.Arrays;
 public class CheckInputAndAnswer {
     // return true is same, return false if different
-    public static boolean checkInputAndAnswer(String input, String answer) {
+    public static boolean checkIfInputAndAnswerAreSame(String input, String answer) {
        if (input == answer) {
            return true;
        }
        return false;
+    }
+
+    public static int strikeCheck(String input, String answer) {
+        int count = 0;
+        char [] inputArr = input.toCharArray();
+        char [] answerArr = answer.toCharArray();
+        for (int i=0; i < 3; i++) {
+            if (inputArr[i] == answerArr[i])
+                count++;
+        }
+        return count;
+    }
+
+    public static int ballCheck(String input, String answer, int strikeCount) {
+        int count = 0;
+        char [] inputArr = input.toCharArray();
+        char [] answerArr = answer.toCharArray();
+        Arrays.sort(inputArr);
+        Arrays.sort(answerArr);
+        for (int i=0; i < 3; i++) {
+            if (inputArr[i] == answerArr[i])
+                count++;
+        }
+        count = (count-strikeCount);
+        return count;
     }
 
 }
