5852cf86a8fc08f76e3b0aa7a68343054005c3c3
besforyou <besforyou2@gmail.com>
2021-02-16 23:15:54 -0800

    feat(src/main/java/baseball/) method to compare input string and answer string
    need to compare input string and answer string.
    Returns true if same, returns false if not same.

---
+++ src/main/java/baseball/CheckInputAndAnswer.java
@@ -0,0 +1,12 @@
+package baseball;
+
+public class CheckInputAndAnswer {
+    // return true is same, return false if different
+    public static boolean checkInputAndAnswer(String input, String answer) {
+        if (input == answer) {
+            return true;
+        }
+        return false;
+    }
+
+}
