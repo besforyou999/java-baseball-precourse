a088ed895150be2e237abe2d6cddd60bef1df6b4
besforyou <besforyou2@gmail.com>
2021-02-18 04:29:00 -0800

    feat(src/main/java/baseball/QuitOrRestart.java) class to ask restart or not
    need a function to ask if the user is going to restart the game
    and check if the given input from user is either 1 or 2

---
+++ src/main/java/baseball/QuitOrRestart.java
@@ -0,0 +1,41 @@
+package baseball;
+
+import java.util.Scanner;
+
+public class QuitOrRestart {
+
+    public static final String WRONG_ARGUMENT_ERROR = "1 또는 2 입력";
+
+    // return 0 if error occured , returns 1 or 2 if input is valid
+    public static int quitOrRestart(Scanner sc) {
+        Question.askReGame();
+        Integer a =0;
+        do {
+            try {
+                String input = sc.nextLine();
+                a = checkUserInput(input);
+            } catch (IllegalArgumentException e) {
+                System.out.println(e.getMessage());
+            }
+        }while(a==0);
+        return a;
+    }
+
+    public static int checkUserInput(String input) {
+        try {
+            return checkValidInteger(input);
+        } catch (NumberFormatException e) {
+            throw new IllegalArgumentException(WRONG_ARGUMENT_ERROR);
+        }
+    }
+
+    public static int checkValidInteger(String input) {
+        int a = Integer.parseInt(input);
+        if ( a != 2 && a != 1) {
+            throw new IllegalArgumentException(WRONG_ARGUMENT_ERROR);
+        }
+        return a;
+    }
+
+
+}
