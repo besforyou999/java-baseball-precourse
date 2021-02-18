b7706dbcf0dd4bf870c46123b7d3b51121cd3725
besforyou <besforyou2@gmail.com>
2021-02-17 23:24:50 -0800

    refactor(/src/main/java/baseball/CheckInputAndAnswer.java) modify method ballCheck
    the recent method ballCheck could not check how many balls occured
    between answer string and input string. By adding a new method
    "checkExistenceOfChar(...)", now can count correct number of balls
    between input string and answer string

--- src/main/java/baseball/CheckInputAndAnswer.java
+++ src/main/java/baseball/CheckInputAndAnswer.java
@@ -1,10 +1,10 @@
 package baseball;
-import java.util.Arrays;
+
 public class CheckInputAndAnswer {
     //return true if input and answer is same, return false if input and answer is not same
     public static boolean checkInputAndAnswer(String input, String answer) {
         int strikeCount = strikeCheck(input,answer);
-        int ballCount = ballCheck(input,answer,strikeCount);
+        int ballCount = ballCheck(input,answer);
         MakeHint.makeHint(strikeCount,ballCount);
         if (strikeCount == 3) {
             return true;
@@ -23,18 +23,23 @@ public class CheckInputAndAnswer {
         return count;
     }
 
-    public static int ballCheck(String input, String answer, int strikeCount) {
+    public static int ballCheck(String input, String answer) {
         int count = 0;
         char [] inputArr = input.toCharArray();
         char [] answerArr = answer.toCharArray();
-        Arrays.sort(inputArr);
-        Arrays.sort(answerArr);
         for (int i=0; i < 3; i++) {
-            if (inputArr[i] == answerArr[i])
-                count++;
+            char checkChar = inputArr[i];
+            count += checkExistenceOfChar(checkChar,answerArr,inputArr,i);
         }
-        count = (count-strikeCount);
         return count;
+    }
+
+    public static int checkExistenceOfChar(char checkChar, char [] answerArr, char [] inputArr, int index) {
+        int exist = 0;
+        for(int i=0; i < 3; i++) {
+            if (checkChar == answerArr[i] && i != index && inputArr[i] != checkChar) exist = 1;
+        }
+        return exist;
     }
 
 }
