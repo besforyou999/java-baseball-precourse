41a1306979a473217f14b8f74122075825a24cab
besforyou <besforyou2@gmail.com>
2021-02-18 04:40:11 -0800

    feat(src/main/java/baseball/CheckInputStringValidity.java) add exception control class
    need to control exceptions when unvalid input has been given from
    user. All exceptions are controlled.

--- src/main/java/baseball/CheckInputStringValidity.java
+++ src/main/java/baseball/CheckInputStringValidity.java
@@ -2,30 +2,37 @@ package baseball;
 
 public class CheckInputStringValidity {
 
+    public static final String STRING_FORMAT_ERROR ="[ERROR] 숫자를 입력해주세요";
+    public static final String STRING_LENGTH_ERROR ="[ERROR] 길이가 3인 숫자를 입력해주세요";
+
     public static boolean checkString(String input) {
-        if (checkNumeric(input) == false) {
-            System.out.println("[ERROR] 숫자를 입력해주세요");
+       boolean check=false;
+       try{
+           check = checkNumeric(input);
+       } catch(IllegalArgumentException e) {
+           System.out.println(e.getMessage());
+           return false;
+       }
+        try{
+            check = checkRange(input);
+        } catch(IllegalArgumentException e) {
+            System.out.println(e.getMessage());
             return false;
         }
-        if (checkRange(input) == false) {
-            System.out.println("[ERROR] 길이가 3인 숫자를 입력해주세요");
-            return false;
-        }
-        return true;
+        return check;
     }
     //returns true if str is numeric , returns false if str is not numeric
     public static boolean checkNumeric(String str) {
-        if (str.matches("-?\\d+")) {
-            return true;
+        if (!str.matches("-?\\d+")) {
+            throw new IllegalArgumentException(STRING_FORMAT_ERROR);
         }
-        return false;
+        return true;
     }
     // returns true if length is 3 , returns false if length is not 3
     public static boolean checkRange(String str) {
         if (str.length() != 3) {
-            return false;
+            throw new IllegalArgumentException(STRING_LENGTH_ERROR);
         }
         return true;
     }
-
 }
