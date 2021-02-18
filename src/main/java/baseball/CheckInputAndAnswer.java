05e7d902bf9f435ae605f29f1fe0c1273048e109
besforyou <besforyou2@gmail.com>
2021-02-17 22:52:23 -0800

    refactor(src/main/java/baseball/CheckInputAndAnswer.java) change and delete method
    need a method to check if the input is same as answer
    and print out hints about the answer

--- src/main/java/baseball/CheckInputAndAnswer.java
+++ src/main/java/baseball/CheckInputAndAnswer.java
@@ -1,12 +1,15 @@
 package baseball;
 import java.util.Arrays;
 public class CheckInputAndAnswer {
-    // return true is same, return false if different
-    public static boolean checkIfInputAndAnswerAreSame(String input, String answer) {
-       if (input == answer) {
-           return true;
-       }
-       return false;
+    //return true if input and answer is same, return false if input and answer is not same
+    public static boolean checkInputAndAnswer(String input, String answer) {
+        int strikeCount = strikeCheck(input,answer);
+        int ballCount = ballCheck(input,answer,strikeCount);
+        MakeHint.makeHint(strikeCount,ballCount);
+        if (strikeCount == 3) {
+            return true;
+        }
+        return false;
     }
 
     public static int strikeCheck(String input, String answer) {
