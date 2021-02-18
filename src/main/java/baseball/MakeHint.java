535e65437153fd6b26d536182d771033b97108a2
besforyou <besforyou2@gmail.com>
2021-02-17 23:29:25 -0800

    style(src/main/java/baseball/MakeHint.java) add newline at printf
    newlines were not created since there were no newlines in printf
    which made it print messages messy to user.

--- src/main/java/baseball/MakeHint.java
+++ src/main/java/baseball/MakeHint.java
@@ -6,10 +6,10 @@ public class MakeHint {
 
     public static void makeHint(int strikeCount, int ballCount) {
         if (ballCount != 0) {
-            System.out.printf("%d볼 ",ballCount);
+            System.out.printf("%d볼\n",ballCount);
         }
         if (strikeCount != 0) {
-            System.out.printf("%d스트라이크 ",strikeCount);
+            System.out.printf("%d스트라이크\n",strikeCount);
         }
         if ( strikeCount == 0 && ballCount == 0) {
             System.out.println("낫싱");
